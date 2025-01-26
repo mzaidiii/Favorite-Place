ff16f72628bf04885a0fe481500fbe7a06ce02da
mzaidiii <mohdmurtaza153@gmail.com>
2025-01-26 09:20:48 +0530

    required changes

--- lib/screens/new_place.dart
+++ lib/screens/new_place.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
-import 'package:favorite_places/Widget/Image_input.dart';
 import 'package:flutter/material.dart';
+import 'package:favorite_places/Widget/Image_input.dart';
 
 class NewPlace extends StatefulWidget {
   @override
@@ -10,16 +10,22 @@ class NewPlace extends StatefulWidget {
 
 class _NewPlaceState extends State<NewPlace> {
   final TextEditingController _placeController = TextEditingController();
-  File? _seletctedImage;
+  File? _selectedImage;
 
-  var placename = '';
+  void saveBack() {
+    final placeName = _placeController.text;
 
-  void saveback() {
-    placename = _placeController.text;
-    if (placename.isEmpty) {
-      Navigator.of(context).pop();
+    if (placeName.isEmpty || _selectedImage == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please provide both a name and an image!')),
+      );
+      return;
     }
-    Navigator.of(context).pop(placename);
+
+    Navigator.of(context).pop({
+      'name': placeName,
+      'image': _selectedImage,
+    });
   }
 
   @override
@@ -28,32 +34,32 @@ class _NewPlaceState extends State<NewPlace> {
       appBar: AppBar(
         title: Text('Add Your Place'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            controller: _placeController,
-            decoration: InputDecoration(label: Text('Name')),
-            maxLength: 100,
-            onChanged: (value) => placename = value,
-          ),
-          const SizedBox(
-            height: 4,
-          ),
-          ImageInput(
-            onPickedimage: (image) {
-              _seletctedImage = image;
-            },
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          ElevatedButton(
-              onPressed: saveback,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _placeController,
+              decoration: InputDecoration(label: Text('Name')),
+              maxLength: 100,
+            ),
+            const SizedBox(height: 16),
+            ImageInput(
+              onPickedimage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: saveBack,
               child: Text(
                 'Save Place',
                 style: TextStyle(color: Colors.white),
-              ))
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
