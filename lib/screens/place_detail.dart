b8fa4eb86bea1a706c377dfc90981a7205a1500f
mzaidiii <mohdmurtaza153@gmail.com>
2025-01-26 09:22:01 +0530

    displaying the image to the whole screen

--- lib/screens/place_detail.dart
+++ lib/screens/place_detail.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class PlaceDetailScreen extends StatelessWidget {
-  const PlaceDetailScreen({super.key, required this.place});
+  const PlaceDetailScreen(
+      {super.key, required this.place, required this.image});
 
   final String place;
+  final File image;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(place),
       ),
-      body: Center(
-        child: Text('hello'),
+      body: Stack(
+        children: [
+          Image.file(
+            image,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          )
+        ],
       ),
     );
   }
