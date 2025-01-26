34437aae1480e1b6f38bfe840fc277a6bd173214
mzaidiii <mohdmurtaza153@gmail.com>
2025-01-26 08:49:47 +0530

    basic changes

--- lib/models/places.dart
+++ lib/models/places.dart
@@ -1,9 +1,11 @@
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 
 const uuid = Uuid();
 
 class Places {
-  Places({required this.Name}) : id = uuid.v4();
+  Places({required this.Name, required this.image}) : id = uuid.v4();
   final String id;
   final String Name;
+  final File image;
 }
