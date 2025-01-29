d4c7e3e6f9bc49a820704539f68fde24640e140a
mzaidiii <mohdmurtaza153@gmail.com>
2025-01-29 14:36:23 +0530

    model edit to take location

--- lib/models/places.dart
+++ lib/models/places.dart
@@ -3,9 +3,22 @@ import 'dart:io';
 
 const uuid = Uuid();
 
+class PlaceLocation {
+  PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+  final double latitude;
+  final double longitude;
+  final String address;
+}
+
 class Places {
-  Places({required this.Name, required this.image}) : id = uuid.v4();
+  Places({required this.Name, required this.image, required this.location})
+      : id = uuid.v4();
   final String id;
   final String Name;
   final File image;
+  final PlaceLocation location;
 }
