6b431c5600179148b142e7ae55ab614581a93d4e
mzaidiii <mohdmurtaza153@gmail.com>
2025-01-30 01:06:55 +0530

    required changes for the database

--- lib/models/places.dart
+++ lib/models/places.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 const uuid = Uuid();
 
 class PlaceLocation {
-  PlaceLocation({
+  const PlaceLocation({
     required this.latitude,
     required this.longitude,
     required this.address,
@@ -15,10 +15,25 @@ class PlaceLocation {
 }
 
 class Places {
-  Places({required this.Name, required this.image, required this.location})
+  Places(
+      {required this.Name,
+      required this.image,
+      required this.location,
+      String? id})
       : id = uuid.v4();
   final String id;
   final String Name;
   final File image;
   final PlaceLocation location;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': Name,
+      'address': location.address,
+      'latitude': location.latitude,
+      'longitude': location.longitude,
+      'imagePath': image.path,
+    };
+  }
 }
